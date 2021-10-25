793447e221512b6c070b42d849ec2aa70aea263c
Osman Taş <68549050+me-tasosman@users.noreply.github.com>
2021-10-25 16:33:13 +0300

    add formSubmit and removeAll functions

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,29 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './App.css';
 
-const onFormSubmit = (e: React.FormEvent) => {
+const formSubmit = (e: React.FormEvent) => {
   e.preventDefault();
   const target = e.target as typeof e.target & {
     option: { value: string };
   };
   const optionEntered = target.option.value;
-  appData.options.push(optionEntered);
-  console.log('optionEntered :>> ', optionEntered);
+
+  if (optionEntered) {
+    appData.options.push(optionEntered);
+    target.option.value = '';
+  }
+  renderApp();
 };
 
 const appData: { title: string; subTitle: string; options: string[] } = {
   title: 'Indecision App',
   subTitle: 'This is some info',
   options: [],
+};
+
+const removeAll = () => {
+  appData.options.length = 0;
+  renderApp();
 };
 
 function App() {
@@ -25,12 +34,14 @@ function App() {
         <h1>{appData.title}</h1>
         {appData.subTitle && <p>{appData.subTitle}</p>}
         {appData.options.length > 0 ? 'Here are your options' : 'No options'}
+        <p>{appData.options.length}</p>
+        <button onClick={removeAll}>Remove All</button>
         <ol>
           <li>Item 1</li>
           <li>Item 2</li>
           <li>Item 3</li>
         </ol>
-        <form onSubmit={onFormSubmit}>
+        <form onSubmit={formSubmit}>
           <input type="text" name="option" />
           <button>Add Option</button>
         </form>
