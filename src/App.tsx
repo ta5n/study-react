5b27a6525a0a863aadcd0c74b3e59e4b4b0bf7c3
Osman Taş <68549050+me-tasosman@users.noreply.github.com>
2021-10-29 21:48:56 +0300

    add randomChoice

--- src/App.tsx
+++ src/App.tsx
@@ -22,24 +22,44 @@ const appData: { title: string; subTitle: string; options: string[] } = {
   options: [],
 };
 
+const makeDecision = () => {
+  const randomNum = Math.floor(Math.random() * appData.options.length);
+  console.log(randomNum);
+};
 const removeAll = () => {
   appData.options.length = 0;
   renderApp();
 };
+
+const numbers = [42, 99, 999];
+
+function WelcomeMessage() {
+  return <h2>Hey Welcome 🙋‍♂️</h2>;
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>{appData.title}</h1>
+        <WelcomeMessage />
         {appData.subTitle && <p>{appData.subTitle}</p>}
         {appData.options.length > 0 ? 'Here are your options' : 'No options'}
         <p>{appData.options.length}</p>
+        <button disabled={appData.options.length === 0} onClick={makeDecision}>
+          What should I do?
+        </button>
         <button onClick={removeAll}>Remove All</button>
+        {/* {[98, 97, 97, 'Mike Smith', null, undefined, true]} */}
+        {/* {[<p key="11">a</p>, <p key="22">b</p>, <p key="23">c</p>]}
+        {[<p key="1">01</p>, <p key="2">02</p>, <p key="3">03</p>, <p key="4">04</p>]} */}
+        {/* {numbers.map(num => {
+          return <p key={num}>Number: {num}</p>;
+        })} */}
         <ol>
-          <li>Item 1</li>
-          <li>Item 2</li>
-          <li>Item 3</li>
+          {appData.options.map((option, index) => (
+            <li key={index}>{option}</li>
+          ))}
         </ol>
         <form onSubmit={formSubmit}>
           <input type="text" name="option" />
