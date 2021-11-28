0d2db26d2bb910ffa3ab36ab880dd6263f7ad601
Osman Taş <68549050+me-tasosman@users.noreply.github.com>
2021-11-28 15:30:43 +0300

    arrowFunc usage

--- src/index.tsx
+++ src/index.tsx
@@ -1,27 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
 // import App from './App';
-import Indecision from './Indecision';
+// import Indecision from './Indecision';
 // import Count from './Count';
 // import User from './User';
-import reportWebVitals from './reportWebVitals';
-// import ES6Arrow from './playground/es6-arrow';
+import reportWebVitals from './reportWebVitals'
+import ES6Arrow from './playground/es6-arrow'
 // import Visibility from './playground/toggle-visibility';
+
+// sum of three numbers
+const sum = (a: number, b: number, c: number) => a + b + c
 
 ReactDOM.render(
   <React.StrictMode>
-    <Indecision />
+    {/* <Indecision /> */}
     {/* <App /> */}
     {/* <Count /> */}
     {/* <User /> */}
-    {/* <ES6Arrow /> */}
+    <ES6Arrow />
     {/* <Visibility /> */}
   </React.StrictMode>,
   document.getElementById('root')
-);
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals()
